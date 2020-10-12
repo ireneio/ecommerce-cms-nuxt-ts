ab1068b1211cf0fedd3dc91f2e605a890116a52a
MAYOHR\keiko_chuang <keiko_chuang@mayohr.com>
2020-10-12 14:18:27 +0800

    Modify build for testing env

--- store/sys.ts
+++ store/sys.ts
@@ -1,11 +1,13 @@
 import { Module, VuexModule, Action, Mutation } from 'vuex-module-decorators'
 import { $axios } from '~/utils/api'
 import { ProxyRequestObject, ResponseObject } from 'Http'
-
+// import store from './index'
 @Module({
   name: 'sys',
   stateFactory: true,
   namespaced: true
+  // dynamic: true,
+  // store
 })
 export default class SysModule extends VuexModule {
   public permissionList: Array<any> = []
